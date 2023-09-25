26d9d96ef59c4b709d748838e9e4aaa33c215b87
jonatasfernandessilva7 <jonatasfernandes@alu.ufc.br>
2023-09-25 08:36:03 -0300

    adicionando if name main

--- Carro/main.py
+++ Carro/main.py
@@ -1,9 +1,10 @@
 from Carro import  Carro
 
-corolla = Carro("corolla", 2023, "azul", 0)
-corolla.printModelo()
-fusca = Carro("fusca", 1870, "verde", 0)
-fusca.criaPortas(7)
-fusca.acelerar()
-fusca.velocidade = 78
-print(fusca.velocidade)
+if __name__ == '__main__':
+	corolla = Carro("corolla", 2023, "azul", 0)
+	corolla.printModelo()
+	fusca = Carro("fusca", 1870, "verde", 0)
+	fusca.criaPortas(7)
+	fusca.acelerar()
+	fusca.velocidade = 78
+	print(fusca.velocidade)
